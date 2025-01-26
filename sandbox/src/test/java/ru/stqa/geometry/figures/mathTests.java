de9649984d9e4eae105da2d0499ff1433fd551a2
anoli <anoli2004@inbox.lv>
2025-01-26 10:13:01 +0200

    add mathTest

---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/mathTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Test;
+
+public class mathTests {
+    @Test
+    void testDivideByZero() {
+        var x = 1;
+        var y = 0;
+        var z = x / y;
+        System.out.println(z);
+    }
+}
