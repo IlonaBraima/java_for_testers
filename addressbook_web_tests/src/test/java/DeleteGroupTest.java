49dbb2309e3ab252496ed586a0e75dea9ff35a5b
anoli <anoli2004@inbox.lv>
2025-02-01 14:52:05 +0200

    add changes in TestBase class

--- addressbook_web_tests/src/test/java/DeleteGroupTest.java
+++ addressbook_web_tests/src/test/java/DeleteGroupTest.java
@@ -5,23 +5,18 @@ public class DeleteGroupTest extends TestBase {
 
     @Test
     public void deleteGroup() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
+        openGroupPage();
+        if (!isGroupPresent()) {
+            createGroup("group name", "group header", "group footer");
         }
-        if (!isElementPresent(By.name(("selected[]")))) {
-            driver.findElement(By.name("new")).click();
-            driver.findElement(By.name("group_name")).click();
-            driver.findElement(By.name("group_name")).sendKeys("group name");
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).sendKeys("group header");
-            driver.findElement(By.name("group_footer")).click();
-            driver.findElement(By.name("group_footer")).sendKeys("group footer");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.linkText("group page")).click();
-        }
-            driver.findElement(By.name("selected[]")).click();
-            driver.findElement(By.name("delete")).click();
-            driver.findElement(By.linkText("group page")).click();
+        removeGroup();
     }
+
+    private static void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
     }
+
+}
 
