ec747b36ac3ed9f6a751d0a78cf9818df4bed2f1
anoli <anoli2004@inbox.lv>
2025-02-02 21:40:24 +0200

    improving tests: removeUserTest and CreateUserTest

--- addressbook_web_tests/src/test/java/TestBase.java
+++ addressbook_web_tests/src/test/java/TestBase.java
@@ -1,13 +1,81 @@
 import model.GroupData;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestBase {
+public abstract class TestBase {
     protected static WebDriver driver;
+
+    protected static void createUser() {
+      driver.findElement(By.linkText("add new")).click();
+      driver.findElement(By.name("firstname")).click();
+      driver.findElement(By.name("firstname")).sendKeys("First name");
+      driver.findElement(By.name("middlename")).click();
+      driver.findElement(By.name("middlename")).sendKeys("Middle name");
+      driver.findElement(By.name("lastname")).click();
+      driver.findElement(By.name("lastname")).sendKeys("Last name");
+      driver.findElement(By.name("nickname")).click();
+      driver.findElement(By.name("nickname")).sendKeys("Nickname");
+      driver.findElement(By.name("title")).click();
+      driver.findElement(By.name("title")).sendKeys("Title");
+      driver.findElement(By.name("theform")).click();
+      driver.findElement(By.name("company")).click();
+      driver.findElement(By.name("company")).sendKeys("Company");
+      driver.findElement(By.name("address")).click();
+      driver.findElement(By.name("address")).sendKeys("Address");
+      driver.findElement(By.name("mobile")).click();
+      driver.findElement(By.name("home")).click();
+      driver.findElement(By.name("home")).sendKeys("12345");
+      driver.findElement(By.name("mobile")).click();
+      driver.findElement(By.name("mobile")).sendKeys("12345");
+      driver.findElement(By.name("work")).click();
+      driver.findElement(By.name("work")).sendKeys("12354");
+      driver.findElement(By.name("fax")).click();
+      driver.findElement(By.name("fax")).sendKeys("12345");
+      driver.findElement(By.name("email")).click();
+      driver.findElement(By.name("email")).sendKeys("test@test.com");
+      driver.findElement(By.name("email2")).click();
+      driver.findElement(By.name("email2")).sendKeys("test1@test.com");
+      driver.findElement(By.name("email3")).click();
+      driver.findElement(By.name("email3")).sendKeys("test2@test.com");
+      driver.findElement(By.name("homepage")).click();
+      driver.findElement(By.name("homepage")).sendKeys("homepage");
+      driver.findElement(By.name("bday")).click();
+      {
+        WebElement dropdown = driver.findElement(By.name("bday"));
+        dropdown.findElement(By.xpath("//option[. = '1']")).click();
+      }
+      driver.findElement(By.name("bmonth")).click();
+      {
+        WebElement dropdown = driver.findElement(By.name("bmonth"));
+        dropdown.findElement(By.xpath("//option[. = 'January']")).click();
+      }
+      driver.findElement(By.name("byear")).click();
+      driver.findElement(By.name("byear")).sendKeys("1990");
+      driver.findElement(By.name("aday")).click();
+      {
+        WebElement dropdown = driver.findElement(By.name("aday"));
+        dropdown.findElement(By.xpath("//option[. = '1']")).click();
+      }
+      driver.findElement(By.name("amonth")).click();
+      {
+        WebElement dropdown = driver.findElement(By.name("amonth"));
+        dropdown.findElement(By.xpath("//option[. = 'January']")).click();
+      }
+      driver.findElement(By.name("ayear")).click();
+      driver.findElement(By.name("ayear")).sendKeys("2000");
+      driver.findElement(By.name("submit")).click();
+
+    }
+
+    protected static void removeUser() {
+      driver.findElement(By.name("selected[]")).click();
+      driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click ();
+      driver.get("http://localhost/addressbook/");
+      driver.findElement(By.id("MassCB")).click();
+      driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
+
+    }
 
     @BeforeEach
     public void setUp() {
@@ -52,5 +120,30 @@ public class TestBase {
 
     protected boolean isGroupPresent() {
         return isElementPresent(By.name("selected[]"));
+    }
+
+    protected void openAddNewUserPage() {
+      if (! isElementPresent(By.name("submit"))) {
+          driver.get("http://localhost/addressbook/group.php");
+      }
+    }
+
+    protected void openHomePage() {
+      if (!isElementPresent(By.id("MassCB"))) {
+        driver.get("http://localhost/addressbook/");
+      }
+    }
+
+    protected boolean isUserListPresent() {
+      return isElementPresent(By.name("entry"));
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 }
