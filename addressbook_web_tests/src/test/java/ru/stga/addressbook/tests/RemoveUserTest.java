2a0faf05bff3b3fbb92d6e78c2c5ee7dcc2fe969
anoli <anoli2004@inbox.lv>
2025-02-28 22:52:13 +0200

    fix initUserModification

--- addressbook_web_tests/src/test/java/ru/stga/addressbook/tests/RemoveUserTest.java
+++ addressbook_web_tests/src/test/java/ru/stga/addressbook/tests/RemoveUserTest.java
@@ -1,5 +1,7 @@
 package ru.stga.addressbook.tests;
 
+import ru.stga.addressbook.common.CommonFunctions;
+import ru.stga.addressbook.model.GroupData;
 import ru.stga.addressbook.model.UserData;
 
 import org.junit.jupiter.api.Assertions;
@@ -25,6 +27,37 @@ public class RemoveUserTest extends TestBase {
         expectedList.remove(index);
         Assertions.assertEquals(newUsers, expectedList);
     }
+    @Test
+    public void CanRemoveContactFromGroup() {
+        if (app.hbm().getUserCount() == 0) {
+            app.hbm().createUser(new UserData("", "firstname", "", "", "", "", "", ""));
+        }
+        var oldUsers = app.hbm().getUserList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldUsers.size());
+        app.users().removeUser(oldUsers.get(index));
+        var newUsers = app.hbm().getUserList();
+        var expectedList = new ArrayList<>(oldUsers);
+        expectedList.remove(index);
+        Assertions.assertEquals(newUsers, expectedList);
+    }
+//
+//    void canCreateContactInGroup(){
+//        var user = new UserData()
+//                .withFirstName(CommonFunctions.randomString(10))
+//                .withLastName(CommonFunctions.randomString(10))
+//                .withPhoto(randomFile("src/test/resources/images"));
+//        if (app.hbm().getGroupCount() == 0) {
+//            app.hbm().createGroup(new GroupData("", "group name", "group header", "group footer"));
+//        }
+//        var group = app.hbm().getGroupList().get(0);
+//
+//        var oldRelated = app.hbm().getUsersInGroup(group);
+//        app.users().create(user, group);
+//        var newRelated = app.hbm().getUsersInGroup(group);
+//        Assertions.assertEquals(oldRelated.size() + 1, newRelated.size());
+//    }
+
 }
 
 
