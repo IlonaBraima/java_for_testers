3370c2fb9d457dabaf9a0dd5878b637543c4adb1
anoli <anoli2004@inbox.lv>
2025-03-11 22:30:14 +0200

    Interacting with the server via HTTP protocol

--- mantis_test/src/test/java/ru.stqa.mantis/manager/ApplicationManager.java
+++ mantis_test/src/test/java/ru.stqa.mantis/manager/ApplicationManager.java
@@ -10,6 +10,7 @@ public class ApplicationManager {
     private String string;
     private Properties properties;
     private SessionHelper sessionHelper;
+    private HttpSessionHelper httpSessionHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -37,6 +38,17 @@ public class ApplicationManager {
             sessionHelper = new SessionHelper(this);
         }
         return sessionHelper;
+    }
+
+    public HttpSessionHelper http() {
+        if (httpSessionHelper == null) {
+            httpSessionHelper = new HttpSessionHelper(this);
+        }
+        return httpSessionHelper;
+    }
+
+    public String property(String name) {
+        return properties.getProperty(name);
     }
 
 }
