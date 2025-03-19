32f17003660fad00cbf8dc846074323c743d0dfb
anoli <anoli2004@inbox.lv>
2025-03-19 08:50:37 +0200

    SOAP API for managing Mantis bug tracker

---
+++ mantis_test/src/test/java/ru.stqa.mantis/manager/SoapApiHelper.java
@@ -0,0 +1,14 @@
+package ru.stqa.mantis.manager;
+
+import ru.stqa.mantis.model.IssueData;
+
+
+public class SoapApiHelper extends HelperBase {
+
+    public SoapApiHelper(ApplicationManager manager) {
+        super(manager);
+    }
+
+    public void createIssue(IssueData issueData) {
+    }
+}
